cb94045eb38565ec0c87b4fa1a45333a1243891e
softOfAndroid <zxy1988816@163.com>
2018-11-28 15:16:43 +0800

    .

---
+++ src/com/zxy/datastruct/array/三数之和.java
@@ -0,0 +1,71 @@
+package com.zxy.datastruct.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+
+public class 三数之和 {
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> lists = threeSum(nums);
+        for (int i = 0; i < lists.size(); i++) {
+            System.out.println(Arrays.toString(lists.get(i).toArray()));
+        }
+    }
+
+    /**
+     * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，
+     * 使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组
+     * 如：nums = [-1, 0, 1, 2, -1, -4]
+     * [
+     * [-1, 0, 1],
+     * [-1, -1, 2]
+     * ]
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);//[-4, -1, -1, 0, 1, 2]
+        HashSet<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    set.add(list);
+                    System.out.println("i = " + i + " ,j = " + j + " ,k = " + k + " -----> " + Arrays.toString(list.toArray()));
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                    j++;
+                    k--;
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+
+
+    /**
+     * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。
+     * 返回这三个数的和。假定每组输入只存在唯一答案。
+     * 如: 给定数组 nums = [-1，2，1，-4], 和 target = 1.
+     * 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+     */
+    public int threeSumClosest(int[] nums, int target) {
+
+        return 0;
+    }
+}
